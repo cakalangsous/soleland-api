26323fbcc78463685237fd2d0f620db3339998c4
Benkin <bennykindangen.com>
2022-10-01 01:04:48 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const { json } = require('express');
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send('It works')
+})
+
+app.listen(port, () => {
+    console.log(`server running at port ${port}`)
+})
